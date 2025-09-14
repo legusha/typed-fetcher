d9db841f7840d1844f9d26f8bbb95c682ec98032
Vlad <59069503+legusha@users.noreply.github.com>
2025-09-14 16:13:39 +0300

    Added xml http request provider,and bug fixed with ".js" path (#13)
    * TF-8 added xml http request provider
    
    * Bump package version

--- src/http-error/index.ts
+++ src/http-error/index.ts
@@ -1,3 +1,3 @@
 export { HttpErrorJSON } from './HttpErrorJSON';
-export { HTTP_ERROR_CODE } from './HttpError.types.js';
-export { HttpErrorBase } from './HttpErrorBase.js';
+export { HTTP_ERROR_CODE } from './HttpError.types';
+export { HttpErrorBase } from './HttpErrorBase';
